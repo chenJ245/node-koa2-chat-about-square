2cd7616d8dca833d19fb2a20cbf41fd2ca56ee87
chenJ245 <2452930499@qq.com>
2022-09-06 10:53:21 +0800

    koa2-weibo-code

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,22 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true,
+    blogList: [
+      {
+        id: 1,
+        title: 'aaa'
+      },
+      {
+        id: 2,
+        title: 'bbb'
+      },
+      {
+        id: 3,
+        title: 'ccc'
+      }
+    ]
   })
 })
 
@@ -13,6 +28,23 @@ router.get('/string', async (ctx, next) => {
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/profile/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName,
+    pageIndex
   }
 })
 
