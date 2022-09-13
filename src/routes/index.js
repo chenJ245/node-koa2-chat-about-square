70163ee371906e7bd7dcbd1a5d56b38e0140c08a
chenJ245 <2452930499@qq.com>
2022-09-13 22:18:32 +0800

    feat: 抽离登录验证的中间件

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
+const { loginRedirect, loginCheck } = require('../middlewares/loginChecks')
 
-router.get('/', async (ctx, next) => {
+router.get('/', loginRedirect, async (ctx, next) => {
     await ctx.render('index', {
         title: 'Hello Koa 2!',
         isMe: true,
@@ -25,7 +26,7 @@ router.get('/string', async (ctx, next) => {
     ctx.body = 'koa2 string'
 })
 
-router.get('/json', async (ctx, next) => {
+router.get('/json', loginCheck, async (ctx, next) => {
     // const session = ctx.session
     // if (session.viewNum == null) {
     //   session.viewNum = 0
