2cbb2c682067327c54354300a46b94a3b5238ca3
chenJ245 <2452930499@qq.com>
2022-09-16 00:42:27 +0800

    test: 创建微博 单元测试

---
+++ test/testUserInfo.js
@@ -0,0 +1,12 @@
+/**
+ * @description 用户单元测试的用户信息
+ * @author 阿白
+ */
+
+/**
+ * cookie 是 用户的敏感信息，此处只能是测试用户的  cookie
+ * 每次测试用户重新登录， 都需要更新这里的 cookie
+ */
+module.exports = {
+    COOKIE: 'weibo.sid=LvLoPA0MkBrnTiUrxAsVzSquVdY1gEet; weibo.sid.sig=xghYhnaa4s48jaMDSKEXK1cQiLk'
+}
