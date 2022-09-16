b1be29c8139e885817830222cb18734845d728cf
chenJ245 <2452930499@qq.com>
2022-09-16 22:56:34 +0800

    feat: 广场页面和路由

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -7,25 +7,26 @@ const router = require('koa-router')()
 const { loginRedirect } = require('../../middlewares/loginChecks')
 const { getProfileBlogList } = require('../../controller/blog-profile')
 const { isExist } = require('../../controller/user')
- 
+const { getSquareBlogList } = require('../../controller/blog-square')
+
 router.get('/', loginRedirect, async (ctx, next) => {
     await ctx.render('index', {})
 })
- 
+
 // 个人主页
 router.get('/profile', loginRedirect, async (ctx, next) => {
     const { userName } = ctx.session.userInfo
     ctx.redirect(`/profile/${userName}`)
 })
- 
+
 router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
     const myUserInfo = ctx.session.userInfo
     const myUserName = myUserInfo.userName
     const { userName: curUserName } = ctx.params
     const isMe = myUserName === curUserName
- 
+
     let curUserInfo
- 
+
     if (isMe) {
         // 是当前登录用户
         curUserInfo = myUserInfo
@@ -39,10 +40,10 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
         }
         curUserInfo = existUserInfo.data
     }
- 
+    
     const result = await getProfileBlogList(curUserName, 0)
     const { isEmpty, count, pageIndex, pageSize, blogList } = result.data
- 
+
     await ctx.render('profile', {
         blogData: {
             isEmpty,
@@ -57,5 +58,22 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
         }
     })
 })
- 
+
+// 广场
+router.get('/square', loginRedirect, async (ctx, next) => {
+    // 获取微博数据，第一页
+    const result = await getSquareBlogList(0)
+    const { isEmpty, count, blogList, pageSize, pageIndex } = result.data || {}
+
+    await ctx.render('square', {
+        blogData: {
+            isEmpty,
+            count,
+            blogList,
+            pageSize,
+            pageIndex
+        }
+    })
+})
+
 module.exports = router
