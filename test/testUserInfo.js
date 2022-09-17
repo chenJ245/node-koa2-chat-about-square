ffe82a41c03ce3fe70f2a8b5377112056fa1f3af
chenJ245 <2452930499@qq.com>
2022-09-18 02:36:18 +0800

    test: getAtList

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -13,5 +13,6 @@ module.exports = {
     Z_COOKIE: 'weibo.sid=_El7lTTbNmp0zuV3bIwmnOVXSug60zTQ; weibo.sid.sig=ZgGJQKYXJyCWJuxYKvKaT7bs5p4',
 
     L_ID: 2,
-    L_USER_NAME: 'lisi'
+    L_USER_NAME: 'lisi',
+    L_COOKIE: 'weibo.sid=0MKgAcsAmKwDMSGl_pUdV3daCTMuIfMH; weibo.sid.sig=JMS74PuFHzeVLRwCmzhsn4-z3xo'
 }
