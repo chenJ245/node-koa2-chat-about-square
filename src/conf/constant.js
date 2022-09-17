056b3d1d2ff62025692d004ef719d886349becc3
chenJ245 <2452930499@qq.com>
2022-09-18 02:19:08 +0800

    feat: 实现 at 功能

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -5,5 +5,8 @@
 
 module.exports = {
     DEFAULT_PICTURE: 'https://c-ssl.duitang.com/uploads/item/202002/09/20200209122704_vdtid.jpg',
-    PAGE_SIZE: 5
+    PAGE_SIZE: 5,
+
+    // 正则表达式，匹配 '@昵称 - userName'
+    REG_FOR_AT_WHO: /@(.+?)\s-\s(\w+?)\b/g
 }
--- src/services/_format.js
+++ src/services/_format.js
@@ -3,7 +3,7 @@
  * @author 阿白
  */
 
-const { DEFAULT_PICTURE } = require('../conf/constant')
+const { DEFAULT_PICTURE, REG_FOR_AT_WHO } = require('../conf/constant')
 const { timeFormat } = require('../utils/dt')
 
 /**
@@ -47,6 +47,27 @@ function _formatDBTime(obj) {
 }
 
 /**
+ * 格式化微博内容
+ * @param {Object} obj 微博数据对象
+ */
+function _formatContent(obj) {
+    obj.contentFormat = obj.content
+
+    // 格式化 @
+    // from @张三 - zhangsan 你好
+    // to <a href="/profile/zhangsan">张三</a> 你好
+    obj.contentFormat = obj.contentFormat.replace(
+        REG_FOR_AT_WHO,
+        (matchStr,nickName, userName) => {
+            return `<a href="/profile/${userName}">@${nickName}</a>`
+        }
+    )
+    return obj
+}
+
+
+
+/**
  * 格式化微博信息
  * @param {Array|Object} list 微博列表或单个微博对象
  */
@@ -55,10 +76,15 @@ function formatBlog(list) {
         return list
     }
     if (list instanceof Array) {
-        return list.map(_formatDBTime)
+        // 数组
+        return list.map(_formatDBTime).map(_formatContent)
     }
+    // 对象
+    let result = list
+    result = _formatDBTime(result)
+    result = _formatContent(result)
 
-    return _formatDBTime(list)
+    return result
 }
 
 module.exports = {
