ca2aba691ead896e2cd3b498ee06d9cf71975c94
chenJ245 <2452930499@qq.com>
2022-09-18 21:16:14 +0800

    feat: 分析并创建 at 关系

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -7,13 +7,35 @@ const xss = require('xss')
 const { createBlog, getFollowersBlogList } = require('../services/blog')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const { createBlogFailInfo } = require('../model/Errorinfo')
-const { PAGE_SIZE } = require('../conf/constant')
+const { PAGE_SIZE, REG_FOR_AT_WHO } = require('../conf/constant')
+const { getUserInfo } = require('../services/user')
+const { createAtRelation } = require('../services/at-relation')
 
 /**
  * 创建微博
  * @param {Object} param0 创建微博所需的数据 { userId, content, image }
  */
 async function create({ userId, content, image }) {
+    // 分析并收集 content 中的 @ 用户
+    // content 格式如 'hi @胡大 - huda 你好 @xx - xx'
+    const atUserNameList = []
+    content = content.replace(
+        REG_FOR_AT_WHO,
+        (matchStr,nickName, userName) => {
+            // 目的不是 replace 而是获取 所有的 userName
+            atUserNameList.push(userName)
+            return matchStr // 替换不生效，预期
+        }
+    )
+
+    // 根据 @ 用户名查询用户信息
+    const atUserList = await Promise.all(
+        atUserNameList.map(userName => getUserInfo(userName))
+    )
+
+    // 根据用户信息，获取用户 id
+    const atUserIdList = atUserList.map(user => user.id)
+
     try {
         // 创建微博
         const blog = await createBlog({
@@ -21,6 +43,13 @@ async function create({ userId, content, image }) {
             content: xss(content),
             image
         })
+
+        // 创建 @ 关系
+        await Promise.all(atUserIdList.map(
+            userId => createAtRelation(blog.id, userId)
+        ))
+
+        // 返回
         return new SuccessModel(blog)
     } catch (ex) {
         console.error(ex.message, ex.stack)
--- src/db/model/index.js
+++ src/db/model/index.js
@@ -6,6 +6,7 @@
 const User = require('./User')
 const Blog = require('./Blog')
 const UserRelation = require('./UserRelation')
+const AtRelation = require('./AtRelation')
 
 Blog.belongsTo(User, {
     foreignKey: 'userId'
@@ -23,8 +24,13 @@ Blog.belongsTo(UserRelation, {
     targetKey: 'followerId'
 })
 
+Blog.hasMany(AtRelation, {
+    foreignKey: 'blogId',
+})
+
 module.exports = {
     User,
     Blog,
-    UserRelation
+    UserRelation,
+    AtRelation
 }   
